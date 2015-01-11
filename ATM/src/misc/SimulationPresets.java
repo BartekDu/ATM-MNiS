64145a04a7c0d142e96fe2949aed015b096ba749
dektrab <a@a-PC>
2015-01-11 19:42:47 +0100

    .TODOs z hintami ..profesjonalny komantarz

--- ATM/src/misc/SimulationPresets.java
+++ ATM/src/misc/SimulationPresets.java
@@ -2,103 +2,113 @@ package misc;
 
 public class SimulationPresets {
 
-    private Integer withdrawLowerLimit;
+	private Integer withdrawLowerLimit;
 
-    private Integer withdrawUpperLimit;
+	private Integer withdrawUpperLimit;
 
-    private Integer clientsPerDay;
+	private Integer clientsPerDayMin;
 
-    private Integer clientsPerSecond;
+	private Integer clientsPerDayMax;
 
-    private Integer PLN200;
+	private Integer clientsPerSecond;
 
-    private Integer PLN100;
+	private Integer PLN200;
 
-    private Integer PLN50;
+	private Integer PLN100;
 
-    private Integer PLN20;
+	private Integer PLN50;
 
-    private Integer PLN10;
+	private Integer PLN20;
 
-    private Integer daysToRefill;
+	private Integer PLN10;
 
-    public int getDaysToRefill() {
-        return daysToRefill;
-    }
+	private Integer daysToRefill;
 
-    public void setDaysToRefill(Integer daysToRefill) {
-        this.daysToRefill = daysToRefill;
-    }
+	public int getDaysToRefill() {
+		return daysToRefill;
+	}
 
-    public int getWithdrawLowerLimit() {
-        return withdrawLowerLimit;
-    }
+	public void setDaysToRefill(Integer daysToRefill) {
+		this.daysToRefill = daysToRefill;
+	}
 
-    public int getWithdrawUpperLimit() {
-        return withdrawUpperLimit;
-    }
+	public int getWithdrawLowerLimit() {
+		return withdrawLowerLimit;
+	}
 
-    public int getClientsPerDay() {
-        return clientsPerDay;
-    }
+	public int getWithdrawUpperLimit() {
+		return withdrawUpperLimit;
+	}
 
-    public int getClientsPerSecond() {
-        return clientsPerSecond;
-    }
+	public int getClientsPerDayMin() {
+		return clientsPerDayMin;
+	}
 
-    public void setWithdrawLowerLimit(String withdrawLowerLimit) {
-        this.withdrawLowerLimit = Integer.parseInt(withdrawLowerLimit);
-    }
+	public int getClientsPerDayMax() {
+		return clientsPerDayMax;
+	}
 
-    public void setWithdrawUpperLimit(String withdrawUpperLimit) {
-        this.withdrawUpperLimit = Integer.parseInt(withdrawUpperLimit);
-    }
+	public int getClientsPerSecond() {
+		return clientsPerSecond;
+	}
 
-    public void setClientsPerDay(String string) {
-        this.clientsPerDay = Integer.parseInt(string);
-    }
+	public void setWithdrawLowerLimit(String withdrawLowerLimit) {
+		this.withdrawLowerLimit = Integer.parseInt(withdrawLowerLimit);
+	}
 
-    public void setClientsPerSecond(String string) {
-        this.clientsPerSecond = Integer.parseInt(string);
-    }
+	public void setWithdrawUpperLimit(String withdrawUpperLimit) {
+		this.withdrawUpperLimit = Integer.parseInt(withdrawUpperLimit);
+	}
 
-    public int getPLN200() {
-        return PLN200;
-    }
+	public void setClientsPerDayMax(String clientsPerDayMax) {
+		this.clientsPerDayMax = Integer.parseInt(clientsPerDayMax);
+	}
 
-    public void setPLN200(Integer pLN200) {
-        PLN200 = pLN200;
-    }
+	public void setClientsPerDayMin(String string) {
+		this.clientsPerDayMin = Integer.parseInt(string);
+	}
 
-    public int getPLN100() {
-        return PLN100;
-    }
+	public void setClientsPerSecond(String string) {
+		this.clientsPerSecond = Integer.parseInt(string);
+	}
 
-    public void setPLN100(Integer pLN100) {
-        PLN100 = pLN100;
-    }
+	public int getPLN200() {
+		return PLN200;
+	}
 
-    public int getPLN50() {
-        return PLN50;
-    }
+	public void setPLN200(Integer pLN200) {
+		PLN200 = pLN200;
+	}
 
-    public void setPLN50(Integer pLN50) {
-        PLN50 = pLN50;
-    }
+	public int getPLN100() {
+		return PLN100;
+	}
 
-    public int getPLN20() {
-        return PLN20;
-    }
+	public void setPLN100(Integer pLN100) {
+		PLN100 = pLN100;
+	}
 
-    public void setPLN20(Integer pLN20) {
-        PLN20 = pLN20;
-    }
+	public int getPLN50() {
+		return PLN50;
+	}
 
-    public int getPLN10() {
-        return PLN10;
-    }
+	public void setPLN50(Integer pLN50) {
+		PLN50 = pLN50;
+	}
 
-    public void setPLN10(Integer pLN10) {
-        PLN10 = pLN10;
-    }
+	public int getPLN20() {
+		return PLN20;
+	}
+
+	public void setPLN20(Integer pLN20) {
+		PLN20 = pLN20;
+	}
+
+	public int getPLN10() {
+		return PLN10;
+	}
+
+	public void setPLN10(Integer pLN10) {
+		PLN10 = pLN10;
+	}
 }
--- ATM/src/view/AppWindowAdmin.java
+++ ATM/src/view/AppWindowAdmin.java
@@ -26,14 +26,14 @@ public class AppWindowAdmin {
 
 	private JTextField upperWithdrawal;
 
-	private JTextField peoplePerDay;
+	private JTextField clientsPerDayMin;
 
-	private JTextField peoplePerSecond;
+	private JTextField clientsPerSecond;
 
 	private Presenter presenter;
 
 	private SimulationPresets simPreset;
-	private JTextField textField;
+	private JTextField clientsPerDayMax;
 	private JFrame frame_1;
 
 	/**
@@ -80,201 +80,203 @@ public class AppWindowAdmin {
 	private void initialize() {
 		frame = createMainFrame();
 		// TODO dlugos spinner dostosowana do int 3 digit
-		JPanel runtime = new JPanel();
-		runtime.setBounds(0, 0, 434, 305);
-		frame_1.getContentPane().add(runtime);
-		runtime.setLayout(null);
+		JPanel panelMain = new JPanel();
+		panelMain.setBounds(0, 0, 434, 305);
+		frame_1.getContentPane().add(panelMain);
+		panelMain.setLayout(null);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setToolTipText("Uzupe\u0142nij warto\u015Bci danych namina\u0142\u00F3w");
-		panel_1.setBorder(new TitledBorder(null, "Dostêpne nomna³y", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.setBounds(21, 11, 149, 244);
-		runtime.add(panel_1);
+		JPanel panelAvNotesSettings = new JPanel();
+		panelAvNotesSettings.setLayout(null);
+		panelAvNotesSettings.setToolTipText("Uzupe\u0142nij warto\u015Bci danych namina\u0142\u00F3w");
+		panelAvNotesSettings.setBorder(new TitledBorder(null, "Dostêpne nomna³y", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
+		panelAvNotesSettings.setBounds(21, 11, 149, 244);
+		panelMain.add(panelAvNotesSettings);
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setLayout(null);
-		panel_2.setBounds(10, 19, 46, 114);
-		panel_1.add(panel_2);
+		JPanel panelNotesLabels = new JPanel();
+		panelNotesLabels.setLayout(null);
+		panelNotesLabels.setBounds(10, 19, 46, 114);
+		panelAvNotesSettings.add(panelNotesLabels);
 
 		JLabel label = new JLabel("200 PLN");
 		label.setBounds(0, 0, 46, 14);
-		panel_2.add(label);
+		panelNotesLabels.add(label);
 
 		JLabel label_1 = new JLabel("100 PLN");
 		label_1.setBounds(0, 25, 46, 14);
-		panel_2.add(label_1);
+		panelNotesLabels.add(label_1);
 
 		JLabel label_2 = new JLabel("50 PLN");
 		label_2.setBounds(0, 50, 46, 14);
-		panel_2.add(label_2);
+		panelNotesLabels.add(label_2);
 
 		JLabel label_3 = new JLabel("20 PLN");
 		label_3.setBounds(0, 75, 46, 14);
-		panel_2.add(label_3);
+		panelNotesLabels.add(label_3);
 
 		JLabel label_4 = new JLabel("10 PLN");
 		label_4.setBounds(0, 100, 46, 14);
-		panel_2.add(label_4);
+		panelNotesLabels.add(label_4);
 
-		JPanel panel_3 = new JPanel();
-		panel_3.setLayout(null);
-		panel_3.setBounds(76, 19, 46, 120);
-		panel_1.add(panel_3);
+		JPanel panelSpinners = new JPanel();
+		panelSpinners.setLayout(null);
+		panelSpinners.setBounds(76, 19, 46, 120);
+		panelAvNotesSettings.add(panelSpinners);
 
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setModel(new SpinnerNumberModel(0, 0, 999, 1));
-		spinner_1.addChangeListener(new ChangeListener() {
+		JSpinner spinner200 = new JSpinner();
+		spinner200.setModel(new SpinnerNumberModel(0, 0, 999, 1));
+		spinner200.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
 
 			}
 		});
-		spinner_1.setBounds(0, 0, 43, 20);
+		spinner200.setBounds(0, 0, 43, 20);
 
-		panel_3.add(spinner_1);
+		panelSpinners.add(spinner200);
 
-		JSpinner spinner_2 = new JSpinner();
-		spinner_2.setModel(new SpinnerNumberModel(0, 0, 999, 1));
-		spinner_2.addChangeListener(new ChangeListener() {
+		JSpinner spinner100 = new JSpinner();
+		spinner100.setModel(new SpinnerNumberModel(0, 0, 999, 1));
+		spinner100.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 			}
 		});
-		spinner_2.setBounds(0, 25, 43, 20);
-		panel_3.add(spinner_2);
+		spinner100.setBounds(0, 25, 43, 20);
+		panelSpinners.add(spinner100);
 
-		JSpinner spinner_3 = new JSpinner();
-		spinner_3.setModel(new SpinnerNumberModel(0, 0, 999, 1));
-		spinner_3.addChangeListener(new ChangeListener() {
+		JSpinner spinner50 = new JSpinner();
+		spinner50.setModel(new SpinnerNumberModel(0, 0, 999, 1));
+		spinner50.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 			}
 		});
-		spinner_3.setBounds(0, 50, 43, 20);
-		panel_3.add(spinner_3);
+		spinner50.setBounds(0, 50, 43, 20);
+		panelSpinners.add(spinner50);
 
-		JSpinner spinner_4 = new JSpinner();
-		spinner_4.setModel(new SpinnerNumberModel(0, 0, 999, 1));
-		spinner_4.addChangeListener(new ChangeListener() {
+		JSpinner spinner20 = new JSpinner();
+		spinner20.setModel(new SpinnerNumberModel(0, 0, 999, 1));
+		spinner20.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 			}
 		});
-		spinner_4.setBounds(0, 75, 43, 20);
-		panel_3.add(spinner_4);
+		spinner20.setBounds(0, 75, 43, 20);
+		panelSpinners.add(spinner20);
 
-		JSpinner spinner_5 = new JSpinner();
-		spinner_5.setModel(new SpinnerNumberModel(0, 0, 999, 1));
-		spinner_5.addChangeListener(new ChangeListener() {
+		JSpinner spinner10 = new JSpinner();
+		spinner10.setModel(new SpinnerNumberModel(0, 0, 999, 1));
+		spinner10.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 			}
 		});
-		spinner_5.setBounds(0, 100, 43, 20);
-		panel_3.add(spinner_5);
+		spinner10.setBounds(0, 100, 43, 20);
+		panelSpinners.add(spinner10);
 
-		JButton button = new JButton("Resetuj");
-		button.addActionListener(new ActionListener() {
+		JButton buttonRestAvNotes = new JButton("Resetuj");
+		buttonRestAvNotes.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				spinner_1.setValue(new Integer(0));
-				spinner_2.setValue(new Integer(0));
-				spinner_3.setValue(new Integer(0));
-				spinner_4.setValue(new Integer(0));
-				spinner_5.setValue(new Integer(0));
+				spinner200.setValue(new Integer(0));
+				spinner100.setValue(new Integer(0));
+				spinner50.setValue(new Integer(0));
+				spinner20.setValue(new Integer(0));
+				spinner10.setValue(new Integer(0));
 
 			}
 		});
-		button.setBounds(16, 144, 89, 23);
-		panel_1.add(button);
+		buttonRestAvNotes.setBounds(16, 144, 89, 23);
+		panelAvNotesSettings.add(buttonRestAvNotes);
 
 		JLabel lblUzupenijPo = new JLabel("Uzupe\u0142nij po");
 		lblUzupenijPo.setBounds(7, 178, 59, 14);
-		panel_1.add(lblUzupenijPo);
+		panelAvNotesSettings.add(lblUzupenijPo);
 
-		JSpinner spinner_6 = new JSpinner();
-		spinner_6.addChangeListener(new ChangeListener() {
+		JSpinner spinnerRefillAfterNDays = new JSpinner();
+		spinnerRefillAfterNDays.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
 			}
 		});
-		spinner_6.setBounds(67, 175, 38, 20);
-		panel_1.add(spinner_6);
+		spinnerRefillAfterNDays.setBounds(67, 175, 38, 20);
+		panelAvNotesSettings.add(spinnerRefillAfterNDays);
 
 		JLabel lblDniach = new JLabel("dniach");
 		lblDniach.setBounds(106, 178, 46, 14);
-		panel_1.add(lblDniach);
+		panelAvNotesSettings.add(lblDniach);
 
-		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(190, 11, 234, 68);
-		runtime.add(panel_5);
-		panel_5.setBorder(new TitledBorder(null, "Limit wyp³aty", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_5.setLayout(null);
+		JPanel panelWithdrawalLimits = new JPanel();
+		panelWithdrawalLimits.setBounds(190, 11, 234, 68);
+		panelMain.add(panelWithdrawalLimits);
+		panelWithdrawalLimits.setBorder(new TitledBorder(null, "Limit wyp³aty", TitledBorder.LEADING, TitledBorder.TOP,
+				null, null));
+		panelWithdrawalLimits.setLayout(null);
 
 		JLabel lblGrnaGranicaWypaty = new JLabel("Dolny");
 		lblGrnaGranicaWypaty.setBounds(95, 44, 37, 14);
-		panel_5.add(lblGrnaGranicaWypaty);
+		panelWithdrawalLimits.add(lblGrnaGranicaWypaty);
 
 		JLabel lblGrny = new JLabel("G\u00F3rny");
 		lblGrny.setBounds(95, 19, 37, 14);
-		panel_5.add(lblGrny);
+		panelWithdrawalLimits.add(lblGrny);
 
 		lowerWithdrawal = new JTextField();
 		lowerWithdrawal.setBounds(142, 16, 86, 20);
-		panel_5.add(lowerWithdrawal);
+		panelWithdrawalLimits.add(lowerWithdrawal);
 		lowerWithdrawal.setToolTipText("Najni\u017Csza dozwolona kwota wyp\u0142aty");
 		lowerWithdrawal.setText("10");
 		lowerWithdrawal.setColumns(10);
 
 		upperWithdrawal = new JTextField();
 		upperWithdrawal.setBounds(142, 41, 86, 20);
-		panel_5.add(upperWithdrawal);
+		panelWithdrawalLimits.add(upperWithdrawal);
 		upperWithdrawal.setText("3000");
 		upperWithdrawal.setColumns(10);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(190, 90, 234, 74);
-		runtime.add(panel);
-		panel.setBorder(new TitledBorder(null, "Iloœæ klientow na sekundê", TitledBorder.LEADING, TitledBorder.TOP,
-				null, null));
-		panel.setLayout(null);
+		JPanel panelClientsPerSecondLimits = new JPanel();
+		panelClientsPerSecondLimits.setBounds(190, 90, 234, 74);
+		panelMain.add(panelClientsPerSecondLimits);
+		panelClientsPerSecondLimits.setBorder(new TitledBorder(null, "Iloœæ klientow na sekundê", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
+		panelClientsPerSecondLimits.setLayout(null);
 
 		JLabel lblLiczbaKlijentwNa = new JLabel("Minimalna");
-		lblLiczbaKlijentwNa.setBounds(66, 19, 66, 14);
-		panel.add(lblLiczbaKlijentwNa);
-
-		peoplePerDay = new JTextField();
-		peoplePerDay.setBounds(142, 16, 86, 20);
-		panel.add(peoplePerDay);
-		peoplePerDay.setText("100");
-		peoplePerDay.setColumns(10);
+		lblLiczbaKlijentwNa.setBounds(76, 19, 56, 14);
+		panelClientsPerSecondLimits.add(lblLiczbaKlijentwNa);
+		// TODO pawel tu masz te <x,y> uzywaj
+		clientsPerDayMin = new JTextField();
+		clientsPerDayMin.setBounds(142, 16, 86, 20);
+		panelClientsPerSecondLimits.add(clientsPerDayMin);
+		clientsPerDayMin.setText("100");
+		clientsPerDayMin.setColumns(10);
 
 		JLabel lblMaksymalna = new JLabel("Maksymalna");
 		lblMaksymalna.setBounds(66, 50, 66, 14);
-		panel.add(lblMaksymalna);
+		panelClientsPerSecondLimits.add(lblMaksymalna);
 
-		textField = new JTextField();
-		textField.setBounds(142, 47, 86, 20);
-		panel.add(textField);
-		textField.setText("100");
-		textField.setColumns(10);
+		clientsPerDayMax = new JTextField();
+		clientsPerDayMax.setBounds(142, 47, 86, 20);
+		panelClientsPerSecondLimits.add(clientsPerDayMax);
+		clientsPerDayMax.setText("100");
+		clientsPerDayMax.setColumns(10);
 
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(190, 175, 234, 43);
-		runtime.add(panel_6);
-		panel_6.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_6.setLayout(null);
+		JPanel panelOtherConstants = new JPanel();
+		panelOtherConstants.setBounds(190, 175, 234, 43);
+		panelMain.add(panelOtherConstants);
+		panelOtherConstants.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelOtherConstants.setLayout(null);
 
 		JLabel lblIloKlientowNa = new JLabel("Ilo\u015B\u0107 klientow na sekund\u0119");
 		lblIloKlientowNa.setBounds(6, 16, 126, 14);
-		panel_6.add(lblIloKlientowNa);
+		panelOtherConstants.add(lblIloKlientowNa);
 
-		peoplePerSecond = new JTextField();
-		peoplePerSecond.setBounds(142, 16, 86, 20);
-		panel_6.add(peoplePerSecond);
-		peoplePerSecond.setText("100");
-		peoplePerSecond.setColumns(10);
+		clientsPerSecond = new JTextField();
+		clientsPerSecond.setBounds(142, 16, 86, 20);
+		panelOtherConstants.add(clientsPerSecond);
+		clientsPerSecond.setText("100");
+		clientsPerSecond.setColumns(10);
 
 		JButton btnWstrzymaj = new JButton("Wstrzymaj");
 		btnWstrzymaj.setBounds(216, 248, 89, 27);
@@ -286,31 +288,32 @@ public class AppWindowAdmin {
 
 			}
 		});
-		runtime.add(btnWstrzymaj);
+		panelMain.add(btnWstrzymaj);
 
-		JButton btnNewButton_1 = new JButton("Wzn\u00F3w");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton buttonWznow = new JButton("Wzn\u00F3w");
+		buttonWznow.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
+				// TODO tutaj masz co sie dzieje po nacisnieciu wznow
 				simPreset = new SimulationPresets();
-				simPreset.setClientsPerDay(peoplePerDay.getText());
-				simPreset.setClientsPerSecond(peoplePerSecond.getText());
+				simPreset.setClientsPerDayMin(clientsPerDayMin.getText());
+				simPreset.setClientsPerDayMax(clientsPerDayMax.getText());
+				simPreset.setClientsPerSecond(clientsPerSecond.getText());
 				simPreset.setWithdrawLowerLimit(lowerWithdrawal.getText());
 				simPreset.setWithdrawUpperLimit(upperWithdrawal.getText());
-				simPreset.setPLN200((Integer) spinner_1.getValue());
-				simPreset.setPLN100((Integer) spinner_2.getValue());
-				simPreset.setPLN50((Integer) spinner_3.getValue());
-				simPreset.setPLN20((Integer) spinner_4.getValue());
-				simPreset.setPLN10((Integer) spinner_5.getValue());
-				simPreset.setDaysToRefill((Integer) spinner_6.getValue());
+				simPreset.setPLN200((Integer) spinner200.getValue());
+				simPreset.setPLN100((Integer) spinner100.getValue());
+				simPreset.setPLN50((Integer) spinner50.getValue());
+				simPreset.setPLN20((Integer) spinner20.getValue());
+				simPreset.setPLN10((Integer) spinner10.getValue());
+				simPreset.setDaysToRefill((Integer) spinnerRefillAfterNDays.getValue());
 				presenter.setSimulationSettings(simPreset);
 				presenter.startSimulation();
 
 			}
 		});
-		btnNewButton_1.setBounds(321, 248, 89, 27);
-		runtime.add(btnNewButton_1);
+		buttonWznow.setBounds(321, 248, 89, 27);
+		panelMain.add(buttonWznow);
 
 	}
 
