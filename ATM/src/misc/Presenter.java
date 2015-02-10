6497a90866712077b8550b3b07338b2870654cea
dektrab <bartekd11@gmail.com>
2015-02-10 22:14:05 +0100

    .

--- ATM/src/misc/Presenter.java
+++ ATM/src/misc/Presenter.java
@@ -1,48 +1,53 @@
 package misc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import simulator.SimulationController;
 import view.AppWindowAdmin;
 
 public class Presenter {
-    private SimulationController simulationController;
+	private SimulationController simulationController;
 
-    private AppWindowAdmin window;
+	private AppWindowAdmin window;
 
-    public Presenter(SimulationController simulationController) {
-        this.simulationController = simulationController;
-    }
+	public Presenter(SimulationController simulationController) {
+		this.simulationController = simulationController;
+	}
 
-    public Presenter() {
-        // TODO Auto-generated constructor stub
-    }
+	public Presenter() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public void startSimulation() {
-        simulationController.startSimulation();
-    }
+	public void startSimulation() {
+		simulationController.startSimulation();
+	}
 
-    public void setSimulationSettings(SimulationPresets simulationPresets) {
+	public void setSimulationSettings(SimulationPresets simulationPresets) {
 
-        simulationController.prepareSimulation(simulationPresets);
-    }
+		simulationController.prepareSimulation(simulationPresets);
+	}
 
-    public void stopSimulation() {
-        simulationController.stopSimulation();
-    }
+	public void stopSimulation() {
+		simulationController.stopSimulation();
+	}
 
-    public void setElapsedDay(int i) {
-        window.setTimer(i);
+	public void setElapsedDay(int i) {
+		window.setTimer(i);
 
-    }
+	}
 
-    public List<Integer> getHistory() {
-        return simulationController.getBalanceHistory();
-    }
+	public List<Integer> getHistory() {
+		List<Integer> l = new ArrayList<Integer>();
+		for (Integer integer : simulationController.getBalanceHistory()) {
+			l.add(integer);
+		}
+		return l;
+	}
 
-    public void setGui(AppWindowAdmin window) {
-        this.window = window;
+	public void setGui(AppWindowAdmin window) {
+		this.window = window;
 
-    }
+	}
 
 }
--- ATM/src/view/SomeWindow.java
+++ ATM/src/view/SomeWindow.java
@@ -1,10 +1,13 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 
 public class SomeWindow extends JDialog {
-
+	private List<Integer> graphData = new ArrayList<Integer>();
 	private JFrame frame;
 
 	/**
@@ -13,9 +16,13 @@ public class SomeWindow extends JDialog {
 
 	/**
 	 * Create the application.
+	 * 
+	 * @param withdrawalHistory
 	 */
-	public SomeWindow() {
+	public SomeWindow(List<Integer> withdrawalHistory) {
+		this.graphData = withdrawalHistory;
 		initialize();
+
 	}
 
 	/**
@@ -25,7 +32,7 @@ public class SomeWindow extends JDialog {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new Chart());
+		frame.add(new Chart(graphData));
 		frame.pack();
 
 	}
